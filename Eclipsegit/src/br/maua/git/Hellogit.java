19ac2084be8e12820d0c22805b8ea345444dbb66
Willian Chan <36850947+willianchan@users.noreply.github.com>
2019-03-09 12:22:56 -0300

    Update Hellogit.java

--- Eclipsegit/src/br/maua/git/Hellogit.java
+++ Eclipsegit/src/br/maua/git/Hellogit.java
@@ -5,6 +5,8 @@ public class Hellogit {
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 		System.out.println("Hello git...");
+		
+		System.out.println("HELLO GIT - ALTERADO NO GITHUB...");
 	}
 
 }
